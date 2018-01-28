3ca3f34a531d37dc21911935d2ba164e33c83fd5
Raikul <tenchuu@gmail.com>
2018-01-27 23:32:25 -0300

    Clean unnecesary variables

--- Assets/Scripts/MaskController.cs
+++ Assets/Scripts/MaskController.cs
@@ -6,36 +6,30 @@ public class MaskController : MonoBehaviour {
 
     public float size = 45;
     float fscale;
-    Vector3 scale;
-    Transform tr;
+    Transform myTransform;
     Vector3 v3scale;
 
-    
-
     bool decays = false;
+    private void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
 
-
-
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         fscale = size;
 
         v3scale = new Vector3(fscale, fscale, fscale);
-        tr = this.GetComponent<Transform>();
-        scale = v3scale;
 
-        tr.localScale = v3scale;
+        myTransform.localScale = v3scale;
 
         if (decays)
             FogDecrease(2);
-
     }
 
     void FogDecrease(int factor)
     {
-        size -= Time.deltaTime * factor;
-    }
-
-    
+        if(size > 0)
+            size -= Time.deltaTime * factor;
+    }    
 }
